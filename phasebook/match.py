f06d184ca83376e106d431370754e1bffd8b6b7f
Tyer Leinster Mercado <leinster.tyer12@gmail.com>
2024-06-29 20:54:18 +0800

    optimize algorithm

--- phasebook/match.py
+++ phasebook/match.py
@@ -20,8 +20,5 @@ def match(match_id):
 
 
 def is_match(fave_numbers_1, fave_numbers_2):
-    for number in fave_numbers_2:
-        if number not in fave_numbers_1:
-            return False
-
-    return True
+    set_fave_numbers_1 = set(fave_numbers_1)
+    return all(number in set_fave_numbers_1 for number in fave_numbers_2)
--- phasebook/search.py
+++ phasebook/search.py
@@ -1,30 +1,46 @@
 from flask import Blueprint, request
-
 from .data.search_data import USERS
 
-
 bp = Blueprint("search", __name__, url_prefix="/search")
-
 
 @bp.route("")
 def search():
     return search_users(request.args.to_dict()), 200
 
+def search_users(id=None, name=None, age=None, occupation=None):
+    id = id if id else request.args.get('id')
+    name = name if name else request.args.get('name')
+    age = age if age else request.args.get('age')
+    occupation = occupation if occupation else request.args.get('occupation')
 
-def search_users(args):
-    """Search users database
+    results = []
+    matched_by_id = []
+    matched_by_name = []
+    matched_by_age = []
+    matched_by_occupation = []
 
-    Parameters:
-        args: a dictionary containing the following search parameters:
-            id: string
-            name: string
-            age: string
-            occupation: string
+    if id:
+        for user in USERS:
+            if user['id'] == id:
+                matched_by_id.append(user)
+                break  # Since IDs are unique, break after finding the match
 
-    Returns:
-        a list of users that match the search parameters
-    """
+    for user in USERS:
+        if name and name.lower() in user['name'].lower():
+            matched_by_name.append(user)
+        if occupation and occupation.lower() in user['occupation'].lower():
+            matched_by_occupation.append(user)
+        if age:
+            try:
+                age = int(age)
+                if abs(user['age'] - age) <= 1:
+                    matched_by_age.append(user)
+            except ValueError:
+                pass
 
-    # Implement search here!
+    results.extend(matched_by_id)
+    results.extend([user for user in matched_by_name if user not in results])
+    results.extend([user for user in matched_by_age if user not in results])
+    results.extend([user for user in matched_by_occupation if user not in results])
 
-    return USERS
+    return sorted(results, key=lambda x: (x in matched_by_id, x in matched_by_name, x in matched_by_age, x in matched_by_occupation), reverse=True)
